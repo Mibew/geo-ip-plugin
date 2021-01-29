7663f6342464b6163464ff0ff42748172c5dd632
Fedor A. Fetisov <faf@ossg.ru>
2021-01-29 22:20:06 +0300

    Switch go Gulp 4 for builds

--- gulpfile.js
+++ gulpfile.js
@@ -35,13 +35,13 @@ gulp.task('get-composer', function(callback) {
 });
 
 // Install Composer dependencies
-gulp.task('composer-install', ['get-composer'], function(callback) {
+gulp.task('composer-install', gulp.series('get-composer', function(callback) {
     exec(config.phpBin + ' composer.phar install --no-dev', function(error, stdout, stderr) {
         callback(error ? stderr : null);
     });
-});
+}));
 
-gulp.task('prepare-release', ['composer-install'], function() {
+gulp.task('prepare-release', gulp.series('composer-install', function() {
     var version = require('./package.json').version;
 
     return eventStream.merge(
@@ -51,14 +51,12 @@ gulp.task('prepare-release', ['composer-install'], function() {
             .pipe(tar('geo-ip-plugin-' + version + '.tar'))
             .pipe(gzip())
     )
-    .pipe(chmod(644))
+    .pipe(chmod(0644))
     .pipe(gulp.dest('release'));
-});
+}));
 
 // Builds and packs plugins sources
-gulp.task('default', ['prepare-release'], function() {
-    // The "default" task is just an alias for "prepare-release" task.
-});
+gulp.task('default', gulp.series('prepare-release'));
 
 /**
  * Returns files stream with the plugin sources.
